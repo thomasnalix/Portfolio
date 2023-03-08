9520af4c37985789d8ada1abe9a1bd1690ecca3f
Thomas Nalix <thomas.nalix123@gmail.com>
2023-03-08 19:51:52 +0100

    Project card aura

--- src/components/LanguageCard.vue
+++ src/components/LanguageCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <a class="bg-[#00000030] rounded-2xl border-2 border-[#1b2434] p-2 lg:px-4 hover:bg-[#2a354826] duration-75" :href="language.link">{{ language.name }}</a>
+    <a class="bg-[#26334f73] rounded-2xl border-2 border-[#1b2434] p-2 lg:px-4 hover:bg-[#2a354826] duration-75" :href="language.link">{{ language.name }}</a>
 </template>
 
 <script>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,29 +1,34 @@
 <template>
-  <div class="card duration-300 flex md:h-72 h-full lg:ml-8 items-center shadow-2xl">
-    <div class="folder hidden lg:block relative right-10 w-72 h-52">
-      <div class="absolute overflow-hidden duration-300 w-52 h-52 opacity-50 rounded-2xl">
-        <img class="h-full select-none min-w-fit" :src="image" alt="Project Image">
+  <div class="card p-3 rounded-2xl flex items-center shadow-md h-full duration-30 md:h-72 h-full lg:ml-8 shadow-2xl">
+    <div class="flex z-10 w-full items-center pr-4">
+      <div class="folder hidden lg:block relative right-10 w-72 h-52">
+        <div class="absolute overflow-hidden duration-300 w-52 h-52 opacity-50 rounded-2xl">
+          <img class="h-full select-none min-w-fit" :src="image" alt="Project Image">
+        </div>
+        <div class="subfolder overflow-hidden duration-300 absolute w-52 opacity-70 h-52 bg-slate-500 rounded-2xl">
+          <img class="h-full select-none min-w-fit" :src="image" alt="Project Image">
+        </div>
+        <div class="cover absolute min-w-[208px] duration-300 w-52 h-52 flex items-center justify-center overflow-hidden rounded-2xl shadow-2xl">
+          <img class="h-full min-w-fit" :src="image" alt="Project Image">
+        </div>
       </div>
-      <div class="subfolder overflow-hidden duration-300 absolute w-52 opacity-70 h-52 bg-slate-500 rounded-2xl">
-        <img class="h-full select-none min-w-fit" :src="image" alt="Project Image">
-      </div>
-      <div class="cover absolute min-w-[208px] duration-300 w-52 h-52 flex items-center justify-center overflow-hidden rounded-2xl shadow-2xl">
-        <img class="h-full min-w-fit" :src="image" alt="Project Image">
+      <div class="flex flex-col w-full gap-2">
+        <div class="flex justify-between">
+          <h2 class="text-xl">{{ project.date }}</h2>
+          <a :href="project.link"><img class="w-8 hover:scale-105 duration-100" src="@/assets/img/githubLogo.png" alt="github logo"></a>
+        </div>
+        <h1 class="font-bold text-3xl">{{ project.title }}</h1>
+        <p class="break-words mb-5">{{ project.description }}</p>
+        <div class="flex justify-between md:flex-row md:gap-2 gap-5 flex-col items-center">
+          <div class="flex flex-wrap justify-center md:justify-start gap-2 w-full">
+            <LanguageCard v-for="language in project.languages" :key="language.name" :language="language" />
+          </div>
+          <router-link :to="`/projects/${project.id}`" class="btn">More</router-link>
+        </div>
       </div>
     </div>
-    <div class="flex flex-col w-full gap-2 pr-4">
-      <div class="flex justify-between">
-        <h2 class="text-xl">{{ project.date }}</h2>
-        <a :href="project.link"><img class="w-8 hover:scale-105 duration-100" src="@/assets/img/githubLogo.png" alt="github logo"></a>
-      </div>
-      <h1 class="font-bold text-3xl">{{ project.title }}</h1>
-      <p class="break-words mb-5">{{ project.description }}</p>
-      <div class="flex justify-between md:flex-row md:gap-2 gap-5 flex-col items-center">
-        <div class="flex flex-wrap justify-center md:justify-start gap-2 w-full">
-          <LanguageCard v-for="language in project.languages" :key="language.name" :language="language" />
-        </div>
-        <router-link :to="`/projects/${project.id}`" class="btn">More</router-link>
-      </div>
+    <div class="overflow-hidden bg-white opacity-5 right-0 top-0 absolute w-full h-full rounded-2xl">
+      <img class="blur-3xl saturate-[5] h-full w-full select-none" :src="image" alt="Project Image">
     </div>
   </div>
 </template>
