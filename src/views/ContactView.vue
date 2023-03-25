c143e445fe66a7dfac0cf17fc8fad8bca2f722b9
Jasthom <91681828+Jasthom@users.noreply.github.com>
2023-03-25 09:29:42 +0100

    Debut mail, contenu json

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -3,10 +3,10 @@
   <div class="duration-100 sm:w-3/4 mx-auto w-11/12">
     <h1 class="text-6xl font-bold text-center mb-10 mt-10">Contact</h1>
     <form class="flex flex-col gap-4 w-3/4 md:w-3/6 mx-auto items-center">
-      <input class="field" type="text" maxlength="100" required placeholder="Nom"/>
-      <input class="field" type="email" maxlength="100" required placeholder="Adresse email"/>
+      <input class="field" type="text" v-model="name" name="name" maxlength="100" required placeholder="Nom"/>
+      <input class="field" type="email" v-model="email" name="email" maxlength="100" required placeholder="Adresse email"/>
       <input class="field" type="text" minlength="10" maxlength="150" required placeholder="Objet"/>
-      <textarea class="field h-32" minlength="25" maxlength="2000" required placeholder="Votre message"/>
+      <textarea class="field h-32" v-model="message" name="message" minlength="25" maxlength="2000" required placeholder="Votre message"/>
       <!-- TODO : Ajouter une verification avec un petit calcul-->
       <div class="w-full">
         <input class="btn cursor-pointer" type="submit" value="Envoyer">
@@ -20,10 +20,37 @@
 
 import NavBar from "@/components/NavBar.vue";
 import FooterBox from "@/components/FooterBox.vue";
+import emailjs from 'emailjs-com';
 
 export default {
   name: "ContactView",
-  components: {FooterBox, NavBar}
+  components: {FooterBox, NavBar},
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: ''
+    }
+  },
+  methods: {
+    sendEmail(e) {
+      try {
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target,
+            'YOUR_USER_ID', {
+              name: this.name,
+              email: this.email,
+              message: this.message
+            })
+
+      } catch(error) {
+        console.log({error})
+      }
+      // Reset form field
+      this.name = ''
+      this.email = ''
+      this.message = ''
+    },
+  },
 }
 </script>
 
